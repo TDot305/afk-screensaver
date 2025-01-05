cad3e6341c8e0faf4c9d53271a6110b8ce7d70fb
Tim Baechle <t.baechle1999@googlemail.com>
2025-01-05 23:43:22 +0100

    Slightly restructure AfkScreensaver.java.

--- src/main/java/AfkScreensaver.java
+++ src/main/java/AfkScreensaver.java
@@ -27,19 +27,20 @@ public class AfkScreensaver extends Application {
     private static final String AFK_LOGO_NAME = "afk_logo.png";
 
     private final Dimension screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
+    private final Rectangle boundingBox = new Rectangle(0, 0, this.screenDimensions.getWidth(), this.screenDimensions.getHeight());
+
     /**
      * Default dimensions of AFK logo puck.
      */
     private final Dimension2D defaultRectDimension = new Dimension2D(400, 130);
-    private final Point2D startingCoordinate = new Point2D(
+    private final Point2D defaultStartingCoordinate = new Point2D(
             screenDimensions.getWidth() / 2 - defaultRectDimension.getWidth() / 2,
             screenDimensions.getHeight() / 2 - defaultRectDimension.getHeight() / 2);
+    private final Rectangle rect = new Rectangle(defaultStartingCoordinate.getX(), defaultStartingCoordinate.getY(), defaultRectDimension.getWidth(), defaultRectDimension.getHeight());
 
     private int pixelsToTraversePerSecond = 300;
-    private final Rectangle boundingBox = new Rectangle(0, 0, this.screenDimensions.getWidth(), this.screenDimensions.getHeight());
-    private final Rectangle rect = new Rectangle(startingCoordinate.getX(), startingCoordinate.getY(), defaultRectDimension.getWidth(), defaultRectDimension.getHeight());
-
     private Point2D lastVector;
+    private boolean debugMode = false;
 
     @Override
     public void start(Stage stage) {
@@ -49,7 +50,6 @@ public class AfkScreensaver extends Application {
         stage.setTitle("AFK Screensaver");
 
         // Configure puck.
-        this.rect.setStyle("-fx-stroke: green; -fx-stroke-width: 3;");
         this.rect.setSmooth(true);
 
         URL afkImageResource = getClass().getResource(AfkScreensaver.AFK_LOGO_NAME);
@@ -74,6 +74,12 @@ public class AfkScreensaver extends Application {
             this.rect.setFill(Color.PINK);
         }
 
+        // Placing puck at random position.
+        double randomXPos = Math.random() * (this.boundingBox.getWidth() - this.rect.getWidth());
+        double randomYPos = Math.random() * (this.boundingBox.getHeight() - this.rect.getHeight());
+        this.rect.setX(randomXPos);
+        this.rect.setY(randomYPos);
+
         var group = new Group(rect);
         var scene = new Scene(group);
         scene.setFill(Color.BLACK);
@@ -83,61 +89,69 @@ public class AfkScreensaver extends Application {
         // Attach handlers.
         stage.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
             switch (keyEvent.getCode()) {
-                case UP -> this.pixelsToTraversePerSecond += 20;
+                case UP -> this.pixelsToTraversePerSecond += 20; // TODO Small information overlay.
                 case DOWN -> this.pixelsToTraversePerSecond = Math.max(10, this.pixelsToTraversePerSecond - 20);
+                case F1 -> {
+                    // TODO Background picker.
+                }
                 case F11 -> stage.setFullScreen(!stage.isFullScreen());
+                case F10 -> {
+                    if(!this.debugMode){
+                        // Turn on debug mode.
+                        this.rect.setStyle("-fx-stroke: green; -fx-stroke-width: 3;");
+                    } else {
+                        // Turn off debug mode.
+                        this.rect.setStyle("");
+                    }
+
+                    this.debugMode = !this.debugMode;
+                }
             }
         });
 
 
         Point2D initialVector = Geometrics.getRandomVector();
+        this.lastVector = initialVector;
+
         double collisionT = Geometrics.getMinimalCollisionT(rect, initialVector, boundingBox);
         Point2D collisionLTCoord = new Point2D(rect.getX(), rect.getY()).add(initialVector.multiply(collisionT));
         LOGGER.debug("Initial move: Vector = " + initialVector + " Collision-t = " + collisionT + " Collision-Coord = " + collisionLTCoord);
 
-        var transition = new TranslateTransition(Duration.seconds(collisionLTCoord.distance(rect.getX(), rect.getY()) / pixelsToTraversePerSecond), rect);
+        LOGGER.info("Launching initial transition.");
+        this.launchNewTransition(collisionLTCoord);
+    }
+
+    private void launchReflectionTransition() {
+        // Determine side that has been hit.
+        Point2D reflectionVector = Geometrics.getDeflectionVector(this.rect, this.lastVector, this.boundingBox);
+        this.lastVector = reflectionVector;
+        double collisionT = Geometrics.getMinimalCollisionT(rect, reflectionVector, boundingBox);
+        Point2D collisionLTCoord = new Point2D(rect.getX(), rect.getY()).add(reflectionVector.multiply(collisionT));
+
+        LOGGER.debug("Collision Point Calculation: rect[" + rect.getX() + ", " + rect.getY() + "] + " + collisionT +
+                " * " + reflectionVector + " = " + collisionLTCoord);
+
+        this.launchNewTransition(collisionLTCoord);
+    }
+
+    private void launchNewTransition(Point2D collisionLTCoord){
+        var transition = new TranslateTransition(Duration.seconds(
+                collisionLTCoord.distance(rect.getX(), rect.getY()) / pixelsToTraversePerSecond), rect);
+
         transition.setToX(collisionLTCoord.getX() - rect.getX());
         transition.setToY(collisionLTCoord.getY() - rect.getY());
         transition.setInterpolator(Interpolator.LINEAR);
         transition.setOnFinished(e -> {
+            // Move to new position.
             this.rect.setX(this.rect.getX() + transition.getToX());
             this.rect.setY(this.rect.getY() + transition.getToY());
             this.rect.setTranslateX(0);
             this.rect.setTranslateY(0);
 
-            this.lastVector = initialVector;
-            triggerReflectionTransition();
+            launchReflectionTransition();
         });
 
-        LOGGER.info("Launching initial transition.");
-        transition.play();
-
-    }
-
-    private void triggerReflectionTransition() {
-        // Determine side that has been hit.
-        Point2D reflectionVector = Geometrics.getDeflectionVector(this.rect, this.lastVector, this.boundingBox);
-        double collisionT = Geometrics.getMinimalCollisionT(rect, reflectionVector, boundingBox);
-        Point2D collisionPoint = new Point2D(rect.getX(), rect.getY()).add(reflectionVector.multiply(collisionT));
-
-        LOGGER.debug("Collision Point Calculation: rect[" + rect.getX() + ", " + rect.getY() + "] + " + collisionT + " * " + reflectionVector + " = " + collisionPoint);
-
-        var transition = new TranslateTransition(Duration.seconds(collisionPoint.distance(rect.getX(), rect.getY()) / pixelsToTraversePerSecond), rect);
-        transition.setToX(collisionPoint.getX() - rect.getX());
-        transition.setToY(collisionPoint.getY() - rect.getY());
-        transition.setInterpolator(Interpolator.LINEAR);
-
-        transition.setOnFinished(e -> {
-            this.rect.setX(this.rect.getX() + transition.getToX());
-            this.rect.setY(this.rect.getY() + transition.getToY());
-            this.rect.setTranslateX(0);
-            this.rect.setTranslateY(0);
-
-            this.lastVector = reflectionVector;
-            triggerReflectionTransition();
-        });
         transition.play();
     }
-
 
 }
